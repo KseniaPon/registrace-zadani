51c93781ff4de26ee55486e3d2778b911b47c8ae
KseniaPon <ponomarevaks@gmail.com>
2024-05-02 15:17:50 +0200

    input and submit

--- src/components/Registration/index.tsx
+++ src/components/Registration/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import './index.css'
 
 interface UserDataStructure {
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
  }
 
  interface FormProps {
@@ -12,14 +13,26 @@ interface UserDataStructure {
  }
 
 export const Registration: React.FC<FormProps> = ({onSubmitForm}) => {
+    const [user, setUser] = useState<UserDataStructure>({
+        username: 'User Name',
+        email: 'Email Address',
+        password: 'Password',
+        passwordConfirm: 'Confirm Password',
+    })
     
-    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        })
+    }
     return (
         <form className='form'>
-            <input name='name' value='' />
-            <input name='e-mail' value='' />
-            <input name='password' value='' />
-            <input name='confirm' value='' />
+            <input value={user.username} name='username' onChange={handleChange} />
+            <input value={user.email} name='email' onChange={handleChange}/>
+            <input value={user.password} name='password'  onChange={handleChange}/>
+            <input value={user.passwordConfirm} name='passwordConfirm' onChange={handleChange}/>
+            <button type='submit'>Submit</button>
         </form>
     )
 }
